d06920a0a9ff8b1fdc13ab1f59183e5d575a7926
Mikko Forsström <pekkisx@gmail.com>
2026-08-30 10:22:03 +0300

    Hip hei

--- 02/functions.js
+++ 02/functions.js
@@ -1,6 +1,11 @@
-export const elm = (type) => {
+export const elm = (type, options = {}) => {
   return (...nodes) => {
     const element = document.createElement(type);
+
+    if (options.className) {
+      element.className = options.className;
+    }
+
     element.append(...nodes);
     return element;
   };
