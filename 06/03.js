aebd270ccc73cbea9ca27add8867cc2d703ddcad
Mikko Forsström <pekkisx@gmail.com>
2026-09-05 23:14:19 +0300

    Push it to the limit, all is done

--- 06/03.js
+++ 06/03.js
@@ -43,5 +43,49 @@ Popupin sisällön voi rakentaa htl:llä (bindPopup ottaa vastaan DOM-solmun).
 import L from "leaflet";
 import "leaflet.markercluster";
 import { html } from "htl";
+import { fetcher } from "../functions.js";
 
-// TODO
+const getCourses = () => {
+  return fetcher("./courses.json");
+};
+
+try {
+  const courses = await getCourses();
+  const map = L.map("map").fitBounds(
+    [
+      [59.5, 20.5],
+      [63.8, 30.0],
+    ],
+    {
+      padding: [5, 5],
+    },
+  );
+
+  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution: "© OpenStreetMap",
+  }).addTo(map);
+
+  const cluster = L.markerClusterGroup();
+
+  const markers = [];
+  for (const course of courses) {
+    const popup = html`
+      <h2>${course.course}</h2>
+      <p>🏠 ${course.address}</p>
+
+      <p>☎️ ${course.phone}</p>
+
+      <p>🌐 <a href=${course.web} rel="noopener" target="_blank">${course.web}</a></p>
+      <p>✉️ ${course.email}</p>
+      <p>${course.text}</p>
+    `;
+
+    markers.push(L.marker([course.lat, course.lng]).bindPopup(popup));
+  }
+
+  cluster.addLayers(markers);
+  map.addLayer(cluster);
+} catch (e) {
+  console.error(e);
+  alert("Iik iik! Joku meni vikaan!!1!");
+}
